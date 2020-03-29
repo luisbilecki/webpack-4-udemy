f81a6a06fb1441b86aae981e899a688bf50a5ce0
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-03-29 19:21:50 -0300

    feat: Added section 10 files
    Integration With Bootstrap

---
+++ section-10-integration-with-bootstrap/src/index.js
@@ -0,0 +1,18 @@
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js'
+import Heading from './components/heading/heading.js'
+import _ from 'lodash'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const heading = new Heading()
+heading.render('hello world')
+const helloWorldButton = new HelloWorldButton()
+helloWorldButton.render()
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production mode')
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('Development mode')
+}
+
+console.log(_.upperFirst('index module'))
